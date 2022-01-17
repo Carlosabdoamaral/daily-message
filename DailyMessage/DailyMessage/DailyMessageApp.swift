a3327c8503f33c8ea72ecfe7994ed5bd7b2ee801
Amaralzaao <72256470+Amaralzaao@users.noreply.github.com>
2022-01-17 16:27:37 -0300

    Adding Files

---
+++ DailyMessage/DailyMessage/DailyMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMessageApp.swift
+//  DailyMessage
+//
+//  Created by Carlos Amaral on 17/01/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
